9f9a8c4aa94f589965e9505c3a16c7c8263ecab8
Jim Huang <jim30223@gmail.com>
2023-06-29 09:58:53 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Basic react unit test settings
+
+use vite build react with jest
+
+## Library
+
+- vite
+- jest
+- @testing-library/jest-dom
+- @testing-library/react
+- @testing-library/user-event
+- ts-jest
+- @types/jest
+- ts-node
+- identity-obj-proxy : mock css
+- jest-environment-jsdom
---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+export default {
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "\\.[jt]sx?$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.ts",
+    "\\.(css|scss|sass)$": "identity-obj-proxy",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+};

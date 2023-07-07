dc0ca4c56f3fab220b8522795c645ba91f5f75d0
Jim Huang <jim30223@gmail.com>
2023-07-07 15:48:08 +0800

    chore: mock server worker

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,8 +1,18 @@
 import "@testing-library/jest-dom/extend-expect";
+import { server } from "./mock/server";
 import { enableFetchMocks } from "jest-fetch-mock";
 enableFetchMocks();
 window.alert = jest.fn();
 
+// Establish API mocking before all tests.
+beforeAll(() => server.listen());
+
+// Reset any request handlers that we may add during the tests,
+// so they don't affect other tests.
 afterEach(() => {
   jest.clearAllMocks();
+  server.resetHandlers();
 });
+
+// Clean up after the tests are finished.
+afterAll(() => server.close());
---
+++ mock/handlers.js
@@ -0,0 +1,27 @@
+import { rest } from 'msw'
+
+export const handlers = [
+  // Handles a POST /login request
+  rest.post('/', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        text: 'hello World'
+      }),
+    )
+  }),
+  rest.get('https://pokeapi.co/api/v2/pokemon/', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({
+      results: [
+        { name: 'test1', url: 'https://pokeapi.co/api/v2/pokemon/1' },
+      ]
+    }))
+  }),
+  rest.get('https://pokeapi.co/api/v2/pokemon/test1', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({
+      sprites: {
+        front_shiny: 'https://fakeimg.pl/300/'
+      }
+    }))
+  })
+]

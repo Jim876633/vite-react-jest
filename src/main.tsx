2e9325b51e3dbfb551ff82581f8fc5c31bdcdef0
Jim Huang <jim30223@gmail.com>
2023-07-13 21:26:19 +0800

    fix: add i18n.js at main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { worker } from "../mock/broswer.js";
+import "./i18n.js";
 
 if (process.env.NODE_ENV === "development") {
   worker.start();

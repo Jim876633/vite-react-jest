047f6b3dd7a601f1ef04227e98a10eaa569c935d
Jim Huang <jim30223@gmail.com>
2023-07-14 09:09:48 +0800

    test: reverse to createBrowser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { RouterProvider, createHashRouter } from "react-router-dom";
-import "./App.css";
 import { routerConfig } from "@/router";
-import { ModalContextProvider } from "./context/ModalContext";
-import { Modal } from "./components/Modal";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./App.css";
+import { Modal } from "./components/Modal";
+import { ModalContextProvider } from "./context/ModalContext";
 
-const router = createHashRouter(routerConfig, {
+const router = createBrowserRouter(routerConfig, {
   basename: import.meta.env.BASE_URL,
 });
 const queryClient = new QueryClient();
